14126bc5226836dab03424f50469ecf6648f8e58
David Matthew Mattli <dmm@mattli.us>
2017-09-05 13:05:16 -0500

    Fix elixir warnings by removing unused imports and marking arguments as ignored.

---
+++ apps/exopticon/lib/exopticon/playback_supervisor.ex
@@ -0,0 +1,13 @@
+defmodule Exopticon.PlaybackSupervisor do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_) do
+    children = []
+    supervise(children, strategy: :one_for_one)
+  end
+
+end
