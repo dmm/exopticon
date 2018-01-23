a8b92b16863f3f75e749def52d8d421cf39f8be0
David Matthew Mattli <dmm@mattli.us>
2018-01-23 13:02:21 -0600

    CameraOverlay: Allow user toggle camera overlay with touch but ignore touch when scrolling.

--- apps/exopticon/assets/js/components/camera_overlay.js
+++ apps/exopticon/assets/js/components/camera_overlay.js
@@ -7,24 +7,21 @@ class CameraOverlay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {opacity: 0};
+    this.touchEnabled = false;
+    this.touchMoving = false;
 
     this.mouseEnter = this.mouseEnter.bind(this);
     this.mouseLeave = this.mouseLeave.bind(this);
     this.touchStart = this.touchStart.bind(this);
+    this.touchMove = this.touchMove.bind(this);
+    this.touchEnd = this.touchEnd.bind(this);
   }
 
   mouseEnter() {
-    this.setState({opacity: 1.0});
-  }
-
-  mouseLeave() {
-    this.setState({opacity: 0.0});
-  }
-
-  touchStart(e) {
-    if (e.target !== e.currentTarget && this.state.opacity > 0) {
+    if (this.touchEnabled) {
       return;
     }
+
     if (this.state.opacity != 0.0) {
       this.setState({opacity: 0.0});
     } else {
@@ -32,11 +29,64 @@ class CameraOverlay extends React.Component {
     }
   }
 
+  mouseLeave() {
+    this.setState({opacity: 0.0});
+  }
+
+  touchStart(e) {
+  }
+
+  touchMove() {
+    this.touchMoving = true;
+  }
+
+  touchEnd(e) {
+    this.touchEnabled = true;
+
+    if (this.touchMoving === true) {
+      this.touchMoving = false;
+      return;
+    }
+
+    if (e.target !== e.currentTarget && this.state.opacity < 1.0) {
+      // When the ptz overlay is not shown prevent the button from
+      // getting the click event.
+      e.preventDefault();
+      // Show the overlay
+      this.setState({opacity: 1.0});
+      return;
+    }
+
+    if (e.target !== e.currentTarget) {
+      // Touch event was on a button, ignore it
+      return;
+    } else if (this.state.opacity != 0.0) {
+      this.setState({opacity: 0.0});
+    } else {
+      this.setState({opacity: 1.0});
+    }
+  }
+
   render() {
-    let ptz = undefined;
+    let ptz = (
+      <div className="ptz-controls"
+           onMouseEnter={this.mouseEnter}
+           onMouseLeave={this.mouseLeave}
+           onTouchStart={this.touchStart}
+           onTouchMove={this.touchMove}
+           onTouchEnd={this.touchEnd}
+           />
+    );
+
     if (this.props.camera.hasPtz()) {
       ptz = (
-        <div className="ptz-controls">
+        <div className="ptz-controls"
+             onMouseEnter={this.mouseEnter}
+             onMouseLeave={this.mouseLeave}
+             onTouchStart={this.touchStart}
+             onTouchMove={this.touchMove}
+             onTouchEnd={this.touchEnd}
+             >
           <OverlayButton
             label="◀"
             extraClass="left-arrow"
@@ -56,12 +106,10 @@ class CameraOverlay extends React.Component {
         </div>
       );
     }
-    return ( 
+    return (
         <div style={{opacity: this.state.opacity}}
              className="camera-overlay"
-             onMouseEnter={this.mouseEnter}
-             onMouseLeave={this.mouseLeave}
-             onTouchStart={this.touchStart}>
+             >
         { ptz }
         <div className="camera-name">{this.props.camera.name}</div>
       </div>
