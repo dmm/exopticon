9058a7445a0eeaf70bf8500907b5e616462764b1
David Matthew Mattli <dmm@mattli.us>
2018-01-30 09:20:32 -0600

    CameraPlayer: Change to a single status variable, removing this.playing.

--- apps/exopticon/assets/js/camera_player.js
+++ apps/exopticon/assets/js/camera_player.js
@@ -8,7 +8,6 @@ class CameraPlayer {
     this.camera = camera;
     Object.assign(this, camera);
     this.relativeMoveUrl = `/v1/cameras/${camera.id}/relativeMove`;
-    this.playing = false;
     this.status = 'paused';
     this.channel = channel;
     this.isDrawing = false;
@@ -28,15 +27,15 @@ class CameraPlayer {
 
     this.channel.on('jpg' + this.camera.id.toString(), (data) => {
       this.channel.push("ack", { ts: data.ts });
-      if (this.playing && this.img !== null) {
-        this.status = 'playing';
+      if (this.status !== 'paused' && this.img !== null) {
+        this.setStatus('playing');
         this.renderFrame(this.img, data.frameJpeg);
       }
     });
 
     this.channel.on('subscribe', () => {
       console.log('got subscribe!');
-      if (this.playing === true) {
+      if (this.status !== 'paused') {
         console.log('rejoining!');
         this.playRealtime(this.img, this.startCb);
       }
@@ -77,18 +76,21 @@ class CameraPlayer {
   }
 
   setStatus(newStatus) {
+    const oldStatus = this.status;
     this.status = newStatus;
-    this.statusCallback(newStatus);
+    if (this.oldStatus !== newStatus) {
+      this.statusCallback(newStatus);
+    }
   }
 
   playRealtime(img) {
     this.channel.push('watch' + this.camera.id.toString(), "");
     this.subTimer = setInterval(() => {
-      if (this.playing === true) {
+      if (this.status !== 'paused') {
         this.channel.push('watch' + this.camera.id.toString(), "");
       }
     }, 5000);
-    this.playing = true;
+
     this.setStatus('loading');
     this.img = img;
 
@@ -106,7 +108,6 @@ class CameraPlayer {
     console.log('stopping...' + this.camera.id.toString());
     clearInterval(this.subTimer);
     this.channel.push('close' + this.camera.id.toString(), "");
-    this.playing = false;
     this.setStatus('paused');
     this.img = null;
   }
