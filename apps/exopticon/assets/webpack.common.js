2b8ac5b8156ed9cc1668831f25c8789b7c695e31
David Matthew Mattli <dmm@mattli.us>
2018-03-25 12:23:46 -0500

    exopticon/assets: Add separate dev/prod webpack configs.

--- apps/exopticon/assets/webpack.common.js
+++ apps/exopticon/assets/webpack.common.js
@@ -1,31 +1,17 @@
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CompressionPlugin = require("compression-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 var webpack = require('webpack');
 
 module.exports = {
   entry: ['./js/app.js', './css/app.css'],
-  devtool: 'source-map',
   output: {
     path: __dirname + '/../priv/static/',
     filename: 'js/app.js'
   },
   plugins: [
-
-    new CompressionPlugin({
-      test: /\.js/
-    }),
-
     new ExtractTextPlugin("css/app.css"),
     new CopyWebpackPlugin([{ from: "./static/" }]),
-    /*
-    new UglifyJsPlugin({
-      parallel: true,
-      test: /\.js($|\?)/i,
-      sourceMap: true,
-    }),
-    */
   ],
   module: {
     rules: [
