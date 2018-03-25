a66073a2c59e905dac41c038175a7b1a36fbe859
David Matthew Mattli <dmm@mattli.us>
2018-03-25 12:07:26 -0500

    capture_port: streamed scaled images to clients.

--- apps/exopticon/lib/exopticon/capture_port.ex
+++ apps/exopticon/lib/exopticon/capture_port.ex
@@ -76,12 +76,21 @@ defmodule Exopticon.CapturePort do
 
   # Handle messages from port
   def handle_port_message({%{"jpegFrame" => dec, "pts" => pts}, id, _}) do
+#    ExopticonWeb.Endpoint.broadcast!("camera:stream", "jpg", %{
+#      cameraId: id,
+#      frameJpeg: Msgpax.Bin.new(dec),
+#      pts: pts
+#    })
+  end
+
+  def handle_port_message({%{"jpegFrameScaled" => dec, "pts" => pts, "height" => _height}, id, _}) do
     ExopticonWeb.Endpoint.broadcast!("camera:stream", "jpg", %{
       cameraId: id,
       frameJpeg: Msgpax.Bin.new(dec),
       pts: pts
     })
   end
+
 
   def handle_port_message({
         %{"filename" => filename, "beginTime" => beginTime},
