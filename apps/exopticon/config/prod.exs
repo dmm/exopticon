589af3c09da8be05bab7b464d74206ec4ef1bf37
David Matthew Mattli <dmm@mattli.us>
2018-03-25 12:46:49 -0500

    exopticon: Change config/prod.exs to use environmental variables.

--- apps/exopticon/config/prod.exs
+++ apps/exopticon/config/prod.exs
@@ -17,7 +17,7 @@ config :exopticon, ExopticonWeb.Endpoint,
   load_from_system_env: true,
   url: [host: Map.fetch!(System.get_env(), "EXOPTICON_HOST"),
         port: Map.fetch!(System.get_env(), "PORT")],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
