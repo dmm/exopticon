36fbf2987acd8bf4bb3da6cc5c16ae160e2689db
David Matthew Mattli <dmm@mattli.us>
2018-04-18 15:46:13 -0500

    exopticon/mix.exs: remove call to build javascript so it can be built separately.

--- apps/exopticon/mix.exs
+++ apps/exopticon/mix.exs
@@ -34,7 +34,7 @@ defmodule Exopticon.Mixfile do
       version: "0.0.1",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext, :native_workers, :javascript] ++ Mix.compilers(),
+      compilers: [:phoenix, :gettext, :native_workers] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
