697b87461a736007e40e7fd22793ff7c2090e430
David Matthew Mattli <dmm@mattli.us>
2018-07-03 07:48:44 -0500

    config: show metadata in logs.

--- apps/exopticon/config/config.exs
+++ apps/exopticon/config/config.exs
@@ -18,7 +18,7 @@ config :exopticon, ExopticonWeb.Endpoint,
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: :all
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
