30b9a1bf794c9ea4d3e455db8770a01b93a606fd
David Matthew Mattli <dmm@mattli.us>
2018-12-10 14:18:38 -0600

    build.rs: add rules to rebuild if c sources are changed.

--- build.rs
+++ build.rs
@@ -4,16 +4,24 @@ use std::process::Command;
 
 fn main() {
     askama::rerun_if_templates_changed();
-
-    println!("cargo:rerun-if-changed=\"src/cworkers\"");
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=src/cworkers/captureworker.c");
+    println!("cargo:rerun-if-changed=src/cworkers/exvid.c");
+    println!("cargo:rerun-if-changed=src/cworkers/Makefile");
+    println!("cargo:rerun-if-changed=src/cworkers/mpack.c");
+    println!("cargo:rerun-if-changed=src/cworkers/mpack_frame.c");
+    println!("cargo:rerun-if-changed=src/cworkers/playbackworker.c");
+    println!("cargo:rerun-if-changed=src/cworkers/exlog.h");
+    println!("cargo:rerun-if-changed=src/cworkers/exvid.h");
+    println!("cargo:rerun-if-changed=src/cworkers/mpack-config.h");
+    println!("cargo:rerun-if-changed=src/cworkers/mpack_frame.h");
+    println!("cargo:rerun-if-changed=src/cworkers/mpack.h");
+    println!("cargo:rerun-if-changed=src/cworkers/timing.h");
 
     Command::new("make")
         .current_dir("src/cworkers")
         .status()
         .expect("c worker build failed.");
-
-    println!("cargo:rerun-if-changed=\"web/js\"");
-    println!("cargo:rerun-if-changed=\"web/css\"");
 
     if !cfg!(debug_assertions) {
         Command::new("npm")
