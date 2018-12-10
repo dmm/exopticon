854988ab3c5b657c37a20fadb4fc76c5a60929b1
David Matthew Mattli <dmm@mattli.us>
2018-12-10 09:28:36 -0600

    build.rs: add building of c executables and web assets.

--- build.rs
+++ build.rs
@@ -1,5 +1,32 @@
 extern crate askama;
 
+use std::process::Command;
+
 fn main() {
     askama::rerun_if_templates_changed();
+
+    println!("cargo:rerun-if-changed=\"src/cworkers\"");
+
+    Command::new("make")
+        .current_dir("src/cworkers")
+        .status()
+        .expect("c worker build failed.");
+
+    println!("cargo:rerun-if-changed=\"web/js\"");
+    println!("cargo:rerun-if-changed=\"web/css\"");
+
+    if !cfg!(debug_assertions) {
+        Command::new("npm")
+            .current_dir("web")
+            .arg("install")
+            .status()
+            .expect("fetching web assets failed.");
+
+        Command::new("npm")
+            .current_dir("web")
+            .arg("run")
+            .arg("deploy")
+            .status()
+            .expect("building web assets failed.");
+    }
 }
