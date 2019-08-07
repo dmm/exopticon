6fe629c4ddafe2b4d29a3b864b70731386dbbc67
David Matthew Mattli <dmm@mattli.us>
2019-08-07 03:53:13 -0500

    DbRegistry: add documentation comments.

--- src/db_registry.rs
+++ src/db_registry.rs
@@ -1,3 +1,5 @@
+//! Implements registry for `DbExecutor`
+
 use actix::Addr;
 use std::sync::Mutex;
 
@@ -7,15 +9,25 @@ lazy_static::lazy_static! {
     static ref DBREG: Mutex<Vec<Addr<DbExecutor>>> = Mutex::new(Vec::new());
 }
 
+/// Returns address of `DbExecutor`
+///
+/// # Panics
+///
+/// Panics if called before `set_db()`.
+///
 pub fn get_db() -> Addr<DbExecutor> {
     DBREG
         .lock()
-        .unwrap()
+        .expect("DbRegistry: Unable to lock db registry for retrieval.")
         .last()
-        .expect("DB address not set!")
+        .expect("DbRegistry: DB address not set!")
         .clone()
 }
 
+/// Set address of `DbExecutor`. Must be called before `get_db()`.
 pub fn set_db(db_address: Addr<DbExecutor>) {
-    DBREG.lock().unwrap().push(db_address);
+    DBREG
+        .lock()
+        .expect("DbRegistry: unable to set address")
+        .push(db_address);
 }
