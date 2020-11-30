0e83f65c98056ac809683be2dd7d10df7d683049
David Matthew Mattli <dmm@mattli.us>
2020-11-29 18:21:51 -0600

    chore: Add GPLv3 to exopticon rust source files.

--- exopticon/src/alert_rule_routes.rs
+++ exopticon/src/alert_rule_routes.rs
@@ -1,3 +1,23 @@
+/*
+ * Exopticon - A free video surveillance system.
+ * Copyright (C) 2020 David Matthew Mattli <dmm@mattli.us>
+ *
+ * This file is part of Exopticon.
+ *
+ * Exopticon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exopticon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Exopticon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 use actix_web::{web::Data, web::Json, Error, HttpResponse};
 
 use crate::app::RouteState;
