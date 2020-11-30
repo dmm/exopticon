2f50d1482f0be7196e0ac342c9361d09426caf23
David Matthew Mattli <dmm@mattli.us>
2020-11-30 03:06:21 -0600

    chore: Update workspace GPLv3 headers

--- workers/cworkers/exvid.c
+++ workers/cworkers/exvid.c
@@ -1,22 +1,21 @@
-/*
- * This file is a part of Exopticon, a free video surveillance tool. Visit
- * https://exopticon.org for more information.
+/* * Exopticon - A free video surveillance system.
+ * Copyright (C) 2020 David Matthew Mattli <dmm@mattli.us>
  *
- * Copyright (C) 2018 David Matthew Mattli
+ * This file is part of Exopticon.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
+ * Exopticon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Exopticon is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Exopticon.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 
 #if __STDC_VERSION__ >= 199901L
 #define _XOPEN_SOURCE 600
