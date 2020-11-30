3375e2bf53db289c1268c7cad052b34ec4359134
David Matthew Mattli <dmm@mattli.us>
2020-11-30 02:55:17 -0600

    chore: Add GPLv3 header to playback sources

--- exopticon/src/playback_actor.rs
+++ exopticon/src/playback_actor.rs
@@ -1,20 +1,22 @@
-// This file is a part of Exopticon, a free video surveillance tool. Visit
-// https://exopticon.org for more information.
-//
-// Copyright (C) 2019 David Matthew Mattli
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//use std::process::{Command, Stdio};
+/*
+ * Exopticon - A free video surveillance system.
+ * Copyright (C) 2020 David Matthew Mattli <dmm@mattli.us>
+ *
+ * This file is part of Exopticon.
+ *
+ * Exopticon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exopticon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Exopticon.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 use std::env;
 use std::path::PathBuf;
