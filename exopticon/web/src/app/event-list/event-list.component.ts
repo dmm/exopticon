bcf379fa3b76f2cf1503ebe84bf8d9b81e0bc417
David Matthew Mattli <dmm@mattli.us>
2021-07-22 04:46:59 -0500

    feat: Add EventList component

--- exopticon/web/src/app/event-list/event-list.component.ts
+++ exopticon/web/src/app/event-list/event-list.component.ts
@@ -1,7 +1,35 @@
+/*
+ * Exopticon - A free video surveillance system.
+ * Copyright (C) 2021 David Matthew Mattli <dmm@mattli.us>
+ *
+ * This file is part of Exopticon.
+ *
+ * Exopticon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exopticon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Exopticon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ZonedDateTime } from "@js-joda/core";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { Event, EventService } from "../event.service";
+import { User, UserService } from "../user.service";
+
+enum EventListState {
+  Loading,
+  Loaded,
+}
 
 @Component({
   selector: "app-event-list",
@@ -9,28 +37,94 @@ import { Event, EventService } from "../event.service";
   styleUrls: ["./event-list.component.css"],
 })
 export class EventListComponent implements OnInit {
-  events$: Observable<Event[]>;
+  public eventListState = EventListState;
   public selectedEvent: number = -1;
+  public state: EventListState;
+  public now = ZonedDateTime.now();
 
-  constructor(private eventService: EventService) {}
+  events$: Observable<Event[]>;
+  user$: Observable<User>;
+  offset: number = 0;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private eventService: EventService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
-    this.events$ = this.eventService.getEvents().pipe(
-      map((events) => {
-        let groups = this.groupEvents(events);
-        console.log(groups);
-        return groups;
-      })
-    );
+    this.user$ = this.userService.getUser();
+    this.refreshEvents();
+    this.user$.subscribe((user) => {
+      console.log("USER UPDATED");
+      this.route.queryParams.subscribe((params) => {
+        console.log(params);
+        if (params["offset"]) {
+          console.log("OFFSET UPDATED");
+          this.offset = parseInt(params["offset"], 10);
+          this.refreshEvents();
+        }
+      });
+    });
+    this.refreshEvents();
+  }
+
+  refreshEvents(): void {
+    this.state = EventListState.Loading;
+    this.selectedEvent = -1;
+    this.user$.subscribe((user) => {
+      this.now = ZonedDateTime.now(user.timezone).minusDays(this.offset);
+      let startOfDay = this.now
+        .withHour(0)
+        .withMinute(0)
+        .withSecond(0)
+        .toInstant();
+      let endOfDay = this.now
+        .withHour(23)
+        .withMinute(59)
+        .withSecond(59)
+        .toInstant();
+      this.events$ = this.eventService.getEvents(startOfDay, endOfDay).pipe(
+        map((events) => {
+          let groups = this.groupEvents(events);
+          this.state = EventListState.Loaded;
+          console.log(groups);
+          return groups;
+        })
+      );
+    });
+  }
+
+  addDuration(offset: number): void {
+    if (offset < 0 && this.offset === 0) {
+      return;
+    }
+    this.offset += offset;
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { offset: this.offset },
+      queryParamsHandling: "merge",
+    });
+  }
+
+  resetDuration(): void {
+    this.offset = 0;
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { offset: this.offset },
+      queryParamsHandling: "merge",
+    });
   }
 
   onEventClick(index: number): void {
     this.selectedEvent = index;
   }
 
-  groupEvents(events: Event[]): Array<Event> {
+  groupEventList(events: Event[]): Array<Event[]> {
     let groups = [];
     let activeGroup = new Array<Event>();
+
     events.forEach((e) => {
       if (activeGroup.length == 0) {
         activeGroup.push(e);
@@ -47,19 +141,33 @@ export class EventListComponent implements OnInit {
     if (activeGroup.length > 0) {
       groups.push(activeGroup);
     }
+    return groups;
+  }
 
-    let longestEvents = groups.map((ev) => {
-      return ev.reduce((acc, val) => {
-        if (acc == null) {
-          acc = val;
-        }
-        return val.interval.toDuration().toMillis() >
-          acc.interval.toDuration().toMillis()
-          ? val
-          : acc;
-      }, null);
+  groupEvents(events: Event[]): Array<Event> {
+    let mainGroups = [];
+    let cameraGroups = events.reduce((r, a) => {
+      let arr = r.has(a.cameraId) ? r.get(a.cameraId) : [];
+      arr.push(a);
+      r.set(a.cameraId, arr);
+      return r;
+    }, new Map<number, Event[]>());
+
+    cameraGroups.forEach((value, key) => {
+      let overlapping = this.groupEventList(value);
+      let longestEvents = overlapping.map((ev) => {
+        return ev.reduce((acc, val) => {
+          if (acc == null) {
+            acc = val;
+          }
+          return val.interval.toDuration().toMillis() >
+            acc.interval.toDuration().toMillis()
+            ? val
+            : acc;
+        }, null);
+      });
+      mainGroups = mainGroups.concat(longestEvents);
     });
-
-    return longestEvents;
+    return mainGroups.sort((a, b) => a.beginTime.isBefore(b.beginTime));
   }
 }
--- exopticon/web/src/app/event.service.ts
+++ exopticon/web/src/app/event.service.ts
@@ -1,3 +1,23 @@
+/*
+ * Exopticon - A free video surveillance system.
+ * Copyright (C) 2021 David Matthew Mattli <dmm@mattli.us>
+ *
+ * This file is part of Exopticon.
+ *
+ * Exopticon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exopticon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Exopticon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Instant } from "@js-joda/core";
@@ -40,9 +60,9 @@ export class Event {
 export class EventService {
   constructor(private http: HttpClient) {}
 
-  getEvents(): Observable<Event[]> {
+  getEvents(beginTime: Instant, endTime: Instant): Observable<Event[]> {
     return this.http
-      .get<EventDto[]>(`/v1/events`)
+      .get<EventDto[]>(`/v1/events?begin_time=${beginTime}&end_time=${endTime}`)
       .pipe(map((dtos) => dtos.map((d) => new Event(d))));
   }
 }
