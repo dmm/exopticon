0d22559a0563d387d84c24c3ff664741fc1b6b1e
David Matthew Mattli <dmm@mattli.us>
2021-07-24 05:15:34 -0500

    fix: Add license header

--- exopticon/web/src/app/user.service.ts
+++ exopticon/web/src/app/user.service.ts
@@ -1,4 +1,24 @@
 import { HttpClient } from "@angular/common/http";
+/*
+ * Exopticon - A free video surveillance system.
+ * Copyright (C) 2021 David Matthew Mattli <dmm@mattli.us>
+ *
+ * This file is part of Exopticon.
+ *
+ * Exopticon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exopticon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Exopticon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { Injectable } from "@angular/core";
 import { ZoneId } from "@js-joda/core";
 import "@js-joda/timezone";
