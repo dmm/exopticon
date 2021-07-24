8d5c20e1d323bdb49537f8820c384e72b82a8aac
David Matthew Mattli <dmm@mattli.us>
2021-07-24 12:29:29 -0500

    fix: Add analysis_engines to migrations so they cleanly apply

--- exopticon/migrations/2021-03-05-130021_add_obj_detection_instances/up.sql
+++ exopticon/migrations/2021-03-05-130021_add_obj_detection_instances/up.sql
@@ -1,5 +1,22 @@
 -- Your SQL goes here
 
+INSERT INTO analysis_engines
+( id, name, version, entry_point )
+VALUES
+(1, 'motion', 'v1.0', 'frigate/motion.py' )
+ON CONFLICT DO NOTHING;
+
+INSERT INTO analysis_engines
+( id, name, version, entry_point )
+VALUES
+(2, 'yolov4', 'v1.0', 'yolov4/darknet.py' )
+ON CONFLICT DO NOTHING;
+
+INSERT INTO analysis_engines
+( id, name, version, entry_point )
+VALUES
+(3, 'coral', 'v1.0', 'coral/coral.py' )
+ON CONFLICT DO NOTHING;
 
 INSERT INTO analysis_instances
 (analysis_engine_id, name, max_fps, enabled)
